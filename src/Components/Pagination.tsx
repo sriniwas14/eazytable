de498e9516d1fd87b11cfa42f1f4c1ef084ee5c5
Sriniwas Jha <sriniwasx@gmail.com>
2023-08-24 19:17:12 +0530

    Added Pagination Component

---
+++ src/Components/Pagination.tsx
@@ -0,0 +1,22 @@
+import React, { FC, useEffect, useState } from 'react'
+
+interface PaginationProps {
+    totalItems: number;
+    currentPage: number;
+    itemsPerPage: number;
+    setCurrentPage(page: number): void;
+}
+
+const Pagination: FC<PaginationProps> = ({ totalItems, currentPage, itemsPerPage }) => {
+    const [pageCount, setPageCount] = useState(0)
+
+    useEffect(() => {
+        setPageCount(Math.ceil(totalItems / itemsPerPage));
+    }, [totalItems])
+
+    return (
+        <div>Pagination</div>
+    )
+}
+
+export default Pagination
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-// MyComponent.tsx
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import "./main.scss"
+import Pagination from './Components/Pagination';
 
 interface ColumnType {
     field: string;
@@ -13,35 +13,65 @@ interface RowType {
 }
 
 interface UiTableProps {
+    title: string;
     columns: Array<ColumnType>;
     data: Array<RowType>;
     rowKeyField: string;
     baseClass?: string;
     customClass?: string;
     fullWidth?: boolean;
+    itemsPerPage?: number;
+    pagination?: boolean;
 }
 
-const UiTable: FC<UiTableProps> = ({ columns, data, rowKeyField, baseClass, customClass, fullWidth }) => {
-    return <table className={`${baseClass ? baseClass : (customClass ? customClass : "default")} ${fullWidth ? "rui_fullWidth" : ""}`}>
-        <thead>
-            <tr>
+const UiTable: FC<UiTableProps> = ({ title, columns, data, rowKeyField, baseClass, customClass, fullWidth, itemsPerPage, pagination }) => {
+    const [currentPage, setCurrentPage] = useState(1)
+
+    return <div className={fullWidth ? "_rui_fullWidth" : ""} style={{ display: "table" }}>
+        <div className="_rui_header">
+            <div className='ruiTitle'>
+                {title}
+            </div>
+            <div className='ruiSearch'>
+                <input type='text' placeholder='Search' />
+            </div>
+        </div>
+        <table className={`${baseClass ? baseClass : (customClass ? customClass : "default")} ${fullWidth ? "_rui_fullWidth" : ""}`}>
+            <thead>
+                <tr>
+                    {
+                        columns.map(column => <td width={column.width || 100} key={column.field}>{column.label}</td>)
+                    }
+                </tr>
+            </thead>
+            <tbody>
                 {
-                    columns.map(column => <td width={column.width || 100} key={column.field}>{column.label}</td>)
+                    data.map((row: RowType) =>
+                        <tr key={row[rowKeyField]}>
+                            {
+                                columns.map(column => <td key={`${row[rowKeyField]}-${column.field}`} width={column.width || 100}>{row[column.field]}</td>)
+                            }
+                        </tr>
+                    )
                 }
-            </tr>
-        </thead>
-        <tbody>
-            {
-                data.map((row: RowType) =>
-                    <tr key={row[rowKeyField]}>
-                        {
-                            columns.map(column => <td key={`${row[rowKeyField]}-${column.field}`} width={column.width || 100}>{row[column.field]}</td>)
-                        }
-                    </tr>
-                )
-            }
-        </tbody>
-    </table>;
+            </tbody>
+        </table>
+        <div className="_rui_footer">
+            <div className='fuiFooterTitle'>
+                Showing 5 of 5 Items
+            </div>
+            <div>
+                {
+                    pagination ? <Pagination
+                        totalItems={data.length}
+                        currentPage={1}
+                        setCurrentPage={() => { }}
+                        itemsPerPage={itemsPerPage || 10}
+                    /> : null
+                }
+            </div>
+        </div>
+    </div>
 };
 
 export default UiTable;
